f095cf1f7f308dfe9422ae7144ebbc054a891f81
Prateekpppp <prateekpppp4gmail.com>
2026-02-20 18:58:00 +0530

    up

--- backend/settings.py
+++ backend/settings.py
@@ -57,6 +57,12 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+
 ROOT_URLCONF = 'backend.urls'
 
 TEMPLATES = [
